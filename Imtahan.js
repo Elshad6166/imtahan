68ef90a1a658543459cb8486cec9f6faddde2df8
Elshad6166 <elshad6166@gmail.com>
2016-05-21 16:09:20 +0400

    imtahan 21.05.2016

---
+++ Imtahan.js
@@ -0,0 +1,13 @@
+
+function changeMetin() {
+    document.getElementById("mentiniDeyis").innerHTML = "Metini deyishdim";
+}
+
+
+
+function changeJpg () {
+	
+}
+
+
+<script type="text/javascript" src="Imtahan.js"></script>
